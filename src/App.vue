f8f4986eb776640af62e96b4787709486eafa69e
konglingwen94 <794430774@qq.com>
2020-05-06 19:01:31 +0800

    修改入口文件

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,27 @@
 <template>
   <div id="app">
-    <Header></Header>
-    <router-view></router-view>
-    <Footer></Footer>
+    <header :class="{isHome:$route.name==='Home'}">
+      <Header></Header>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+      <Footer></Footer>
+    </footer>
   </div>
 </template>
 <script>
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+// import Header from "@/components/header";
+// import Footer from "@/components/footer";
 export default {
   name: "App",
   components: {
-    Header,
-    Footer,
+    // Header,
+    // Footer,
   },
 };
 </script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import '@/assets/reset.css'
+import  '@/helper/registerGlobalComponents.js'
+ 
 Vue.config.productionTip = false;
 
 new Vue({
