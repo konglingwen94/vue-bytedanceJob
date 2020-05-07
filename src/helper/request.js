9e328cb9fd18ce75fa559b20d34ddc3d3d918410
konglingwen94 <794430774@qq.com>
2020-05-07 09:19:38 +0800

    创建请求实例

---
+++ src/helper/request.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({ baseURL: "/api" });
+
+instance.interceptors.response.use(
+  (response) => {
+    const { status ,data} = response;
+    if (status === 200) {
+      return Promise.resolve(data);
+    }
+    return Promise.reject(response.data);
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
