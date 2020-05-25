6704e34c36d35e7dbd60752825cc50deabc249d7
konglingwen94 <794430774@qq.com>
2020-05-25 20:31:34 +0800

    完成分页器组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="pagination">
+    <ul class="pagination-list">
+      <li
+        title="上一页"
+        @click="$emit('update:currentPage',Math.max(1,currentPage-1))"
+        class="pagination-item"
+      >
+        <span><</span>
+      </li>
+      <li
+        class="pagination-item"
+        :class="{current:currentPage===item}"
+        v-for="(item,index) in visiblePages"
+        @click="change(item)"
+        :key="index"
+      >
+        <span>{{item}}</span>
+      </li>
+      <li
+        title="下一页"
+        @click="$emit('update:currentPage',Math.min(totalPage,currentPage+1))"
+        class="pagination-item"
+      >
+        <span>></span>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Pagination",
+  props: {
+    total: Number,
+    perPage: {
+      type: Number,
+      default: 10
+    },
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    pagerCount: {
+      type: Number,
+      default: 9
+    }
+  },
+  computed: {
+    totalPage() {
+      return Math.ceil(parseInt(this.total) / this.perPage);
+    },
+    visiblePages() {
+      let pages = [];
+      const currentPage = Math.max(
+        1,
+        Math.min(this.currentPage, this.totalPage)
+      );
+      console.log("currentPage=", currentPage);
+
+      if (this.totalPage <= this.pagerCount) {
+        for (let i = 1; i <= this.totalPage; i++) {
+          pages.push(i);
+        }
+        return pages;
+      }
+
+      if (currentPage >= this.totalPage - 3) {
+        pages.push(1, "...");
+        const minPage = Math.min(currentPage - 2, this.totalPage - 4);
+        for (let i = minPage, len = this.totalPage; i <= len; i++) {
+          pages.push(i);
+        }
+      } else if (currentPage <= 4) {
+        const maxPage = Math.min(Math.max(currentPage + 2, 5), this.totalPage);
+        for (let i = 1; i <= maxPage; i++) {
+          pages.push(i);
+        }
+        pages.push("...", this.totalPage);
+      } else {
+        pages.push(1, "...");
+        for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+          pages.push(i);
+        }
+        pages.push("...", this.totalPage);
+      }
+      return pages;
+    }
+  },
+  methods: {
+    change(num) {
+      if (typeof num !== "number") {
+        return;
+      }
+      this.$emit("current-change", num);
+      this.$emit("update:currentPage", num);
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.pagination-list {
+  display: flex;
+}
+.pagination-item {
+  margin-right: 4px;
+  cursor: pointer;
+  padding: 8px;
+  &:hover {
+    color: @main-color;
+  }
+  &.current {
+    color: @main-color;
+  }
+}
+</style>
