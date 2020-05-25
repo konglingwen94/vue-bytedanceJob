81c1c9d91617138c22f95c47c48cbe11ea112191
konglingwen94 <794430774@qq.com>
2020-05-25 15:59:09 +0800

    添加job路由

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,24 @@ const routes = [
     name: "Home",
     component: ( )=>import(/*webpackChunkName: "Home" */ "@/views/Home.vue")
   },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+  {
+    path: "/products",
+    name: "products",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Products.vue")
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Jobs.vue")
+  }
 ];
 
 const router = new VueRouter({
