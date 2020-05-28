f320ea85cdf0d8b7836916b642b268160fc4ec9b
konglingwen94 <794430774@qq.com>
2020-05-28 09:49:54 +0800

    添加button组件

---
+++ src/components/Bytedance-Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bytedance-button" :class="`bytedance-button-${size}`">
+    <span>
+      <slot>bytedance-button</slot>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "bytedance-button",
+  props: {
+    buttonText: String,
+    size: {
+      type: String,
+      default: "medium",
+      validator(value) {
+        return ["small", "medium", "large"].includes(value);
+      }
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.bytedance-button {
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: @main-color;
+  color: #fff;
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  cursor:pointer;
+  &:hover :after {
+    content: "";
+    position: absolute;
+    background-color: rgba(255, 255, 255, 0.3);
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+  &-small {
+    height: 20px;
+  }
+  &-large {
+    height: 40px;
+  }
+}
+</style>
