28fd17aaf75f7d3d6948702b5e8c7db4618578fc
konglingwen94 <794430774@qq.com>
2020-08-07 21:47:29 +0800

    更改组件注册文件名匹配规则

--- src/helper/registerGlobalComponents.js
+++ src/helper/registerGlobalComponents.js
@@ -6,9 +6,9 @@ const requireComponent = require.context(
   // 其组件目录的相对路径
   '../components',
   // 是否查询其子目录
-  false,
+  true,
   // 匹配基础组件文件名的正则表达式
-  /[A-Z]\w+\.(vue|js)$/
+  /[A-Za-z]\w+\.(vue|js)$/
 )
 
 requireComponent.keys().forEach(fileName => {
