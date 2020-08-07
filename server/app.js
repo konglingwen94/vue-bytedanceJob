70e522bb167059fc512fe6d83feded60183742cc
konglingwen94 <794430774@qq.com>
2020-08-07 16:56:33 +0800

    添加代理服务转发接口中间件

--- server/app.js
+++ server/app.js
@@ -1,12 +1,33 @@
-const path=require('path')
+const path = require("path");
 const express = require("express");
 const history = require("connect-history-api-fallback");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const router = require("./router");
 
 const app = express();
 const bodyParser = require("body-parser");
 
+const proxyOpts = {
+  target: "https://job.bytedance.com",
+  changeOrigin: true,
+  cookieDomainRewrite: "", //去除cookie的默认携带域名
+
+  onProxyRes(proxyResponse, req, res) {
+    if (proxyResponse.headers["set-cookie"]) {
+      // 去除原服务器响应 cookie 的`secure`和`httpOnly`字段
+      const cookies = proxyResponse.headers["set-cookie"].map((cookie) => {
+        return cookie.replace(/; secure/gi, "").replace(/; httpOnly/gi, "");
+      });
+      proxyResponse.headers["set-cookie"] = cookies;
+    }
+  },
+};
+// 使用代理服务转发请求
+app.use("/api/v1", createProxyMiddleware(proxyOpts));
+
 app.use(bodyParser());
+// 自定义路由转发
 app.use("/api", router);
 app.use(
   history({
@@ -16,5 +37,8 @@ app.use(
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(3000, () => {
+  if (process.env.NODE_ENV !== "production") {
+    console.clear();
+  }
   console.log("Server is listening on http://localhost:3000");
 });
