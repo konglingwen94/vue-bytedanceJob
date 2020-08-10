0eb3e1ae0b2dfc9958ec32bbd0c4908d04365c66
konglingwen94 <794430774@qq.com>
2020-08-10 19:19:34 +0800

    按需引入element-ui组件

---
+++ src/helper/registerElementComponents.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import {
+  Input,
+  Form,
+  FormItem,
+  Select,
+  Option,
+  DatePicker,
+  Tabs,
+  TabPane,
+  Upload,
+  Loading,
+  Message,
+} from "element-ui";
+
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(DatePicker);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Upload);
+Vue.use(Loading);
+Vue.use(Message);
--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import Vue from "vue";
+
+// 按需引入 element-ui 组件
+import "element-ui/lib/theme-chalk/index.css";
+import "./helper/registerElementComponents.js";
+
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/style/reset.css";
 import "@/assets/style/global.css";
+
+// 注册自定义组件
 import "@/helper/registerGlobalComponents.js";
+
+// 引入请求实例
 import request from "@/helper/request";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 Vue.prototype.request = request;
-
-
 
 new Vue({
   router,
