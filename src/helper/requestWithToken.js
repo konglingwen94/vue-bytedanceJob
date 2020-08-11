0ce8518cdab74aca1d2f6e09debebbfcf1606282
konglingwen94 <794430774@qq.com>
2020-08-11 10:39:58 +0800

    添加api请求接口

---
+++ src/helper/requestWithToken.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import instance from "./request";
+ 
+
+const requestWithToken = axios.create({
+  baseURL: "/api",
+});
+
+requestWithToken.interceptors.response.use(
+  (response) => {
+    if (response.data) {
+      return Promise.resolve(response.data);
+    }
+    return Promise.resolve(response);
+  },
+  async (error) => {
+    if (error.response) {
+      if (error.response.status === 405) {
+        try {
+          await instance.post("/v1/csrf/token");
+        } catch (error) {
+          return Promise.reject(error);
+        }
+
+        return instance(error.response.config);
+      }
+      if (error.response.data) {
+        return Promise.reject(error.response.data);
+      }
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const fetchProfile = () =>
+  requestWithToken.get("/v1/user/latest/resume");
+
+export const fetchCommonSettings = () =>
+  requestWithToken.get("/v1/common/setting");
+
+export const fetchLoginStatus = () =>
+  requestWithToken.get("/v1/user/mobile/login_status");
+
+export const fetchEmailRegisterStatus = (payload) =>
+  requestWithToken.post("/v1/user/email/register/check", payload);
+
+export const fetchLoginByEmail = (payload) =>
+  requestWithToken.post("/v1/user/email/login", payload);
+
+export const fetchResume = () => requestWithToken.get("/v1/user/latest/resume");
+
+export const fetchLogout = () => requestWithToken.post("/v1/user/logout");
+
+export const fetchSaveResume = (resume_id, payload) =>
+  requestWithToken.post(`/v1/user/resumes/${resume_id}`, payload);
+
+// GET /api/v1/user/latest/resume  获取个人简历
+
+// GET /api/v1/common/setting  获取简历模板编辑配置
+
+// POST /api/v1/user/email/register/check  --data-raw '{"email":"80r4344323@qq.com"}' 检查邮箱是否已注册
+
+// GET  /api/v1/user/profile  获取个人邮箱信息
+
+// GET /api/v1/user/mobile/login_status  检查登录状态
+
+// POST /api/v1/user/email/login  --data-raw '{"email":"44348843@qq.com","password:"jkljklsd"}'  邮箱登录
+
+// POST /api/v1/user/logout  退出登录
+
+// Post /api/v1/user/resumes/6855865337356044552  保存编辑好的简历 --data-raw '{resume:{},resume_id:'',portal_type: 2}'
