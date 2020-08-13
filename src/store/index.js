56f43c4951037bc99d23e3fe779825223cc6e80f
konglingwen94 <794430774@qq.com>
2020-08-13 12:51:26 +0800

    添加退出逻辑

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,23 @@
 import Vue from "vue";
-import { fetchLoginStatus, fetchProfile } from "../helper/requestWithToken";
+import {
+  fetchLoginStatus,
+  fetchProfile,
+  fetchLogout,
+} from "../helper/requestWithToken";
+
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("account", JSON.stringify(state));
+});
+
+window.addEventListener("load", () => {
+  try {
+    var account = JSON.parse(localStorage.getItem("account"));
+  } catch (error) {
+    account = {};
+  }
+  state.isLogin = account.isLogin;
+  state.userInfo = account.userInfo;
+});
 
 const state = Vue.observable({
   isLogin: false,
@@ -15,14 +33,27 @@ const store = {
   setUserInfo(payload) {
     state.userInfo = payload;
   },
+  expireLogin() {
+    state.isLogin = false;
+    state.userInfo = {};
+  },
   requestLoginStatus() {
     return fetchLoginStatus()
       .then((response) => {
         store.setLoginStatus(response.data);
+        return response.data;
       })
-      .catch(Promise.reject(respons));
+      .catch((err) => Promise.reject(err));
   },
-
+  requestLogout() {
+    return fetchLogout()
+      .then((res) => {
+        if (res.data === "success") {
+          store.expireLogin();
+        }
+      })
+      .catch((err) => Promise.reject(err));
+  },
   requestUserInfo() {
     return fetchProfile()
       .then((response) => {
