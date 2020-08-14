085fc3643f5d8282adc2cd7f0b632c8af54797d9
konglingwen94 <794430774@qq.com>
2020-08-14 17:29:48 +0800

    初步完成模板数据渲染

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -22,12 +22,275 @@
         </div>
       </div>
 
-      <div class="profile-content"></div>
+      <div class="resumeForm">
+        <!-- 基本信息 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">基本信息</h2>
+
+          <div class="resumeViewForm">
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">姓名</div>
+              <div class="resumeViewItem__content">{{resumeDetail.name}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">手机号</div>
+              <div class="resumeViewItem__content">{{resumeDetail.mobile_number}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">邮箱</div>
+              <div class="resumeViewItem__content">{{resumeDetail.email}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 工作经历 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">工作经历</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.career_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">公司</div>
+              <div class="resumeViewItem__content">{{item.company}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">起止时间</div>
+              <div
+                class="resumeViewItem__content"
+              >{{item.start_time | formatDate(false)}} ~ {{item.end_time | formatDate(false)}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">职位</div>
+              <div class="resumeViewItem__content">{{item.title}}</div>
+            </div>
+            <div class="resumeViewItem resumeViewItem-desc">
+              <div class="resumeViewItem__label">描述</div>
+              <div class="resumeViewItem__content">{{item.description}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 教育经历 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">教育经历</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.education_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">学校</div>
+              <div class="resumeViewItem__content">{{item.school}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">起止时间</div>
+              <div
+                class="resumeViewItem__content"
+              >{{item.start_time | formatDate(false)}} ~ {{item.end_time | formatDate(false)}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">专业</div>
+              <div class="resumeViewItem__content">{{item.major}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">学历</div>
+              <div class="resumeViewItem__content">{{item.description}}</div>
+            </div>
+          </div>
+        </div>
+
+        <!-- 实习经历 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">实习经历</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.internship_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">公司</div>
+              <div class="resumeViewItem__content">{{item.company}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">起止时间</div>
+              <div
+                class="resumeViewItem__content"
+              >{{item.start_time | formatDate(false)}} ~ {{item.end_time | formatDate(false)}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">职位</div>
+              <div class="resumeViewItem__content">{{item.position}}</div>
+            </div>
+            <div class="resumeViewItem resumeViewItem-desc">
+              <div class="resumeViewItem__label">描述</div>
+              <div class="resumeViewItem__content">{{item.description}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 项目 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">项目</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.project_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">项目名称</div>
+              <div class="resumeViewItem__content">{{item.name}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">起止时间</div>
+              <div
+                class="resumeViewItem__content"
+              >{{item.start_time | formatDate(false)}} ~ {{item.end_time | formatDate(false)}}</div>
+            </div>
+            <div class="resumeViewItem" v-if="item.role">
+              <div class="resumeViewItem__label">角色</div>
+              <div class="resumeViewItem__content">{{item.role}}</div>
+            </div>
+            <div class="resumeViewItem" v-if="item.link">
+              <div class="resumeViewItem__label">项目连接</div>
+              <div class="resumeViewItem__content">
+                <a
+                  target="_blank"
+                  :href="item.link"
+                  class="resumeViewItem__content-link"
+                >{{item.link}}</a>
+              </div>
+            </div>
+            <div class="resumeViewItem resumeViewItem-desc">
+              <div class="resumeViewItem__label">描述</div>
+              <div class="resumeViewItem__content">{{item.description}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 作品 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">作品</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.works_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem" v-if="item.works_attachment">
+              <div class="resumeViewItem__label">作品附件</div>
+              <div class="resumeViewItem__content">
+                <a
+                  :href="item.works_attachment.name"
+                  :download="item.works_attachment.name"
+                  class="resumeViewItem__content-download"
+                >{{item.works_attachment.name}}</a>
+              </div>
+            </div>
+            <div class="resumeViewItem" v-if="item.link">
+              <div class="resumeViewItem__label">作品链接</div>
+              <div class="resumeViewItem__content">
+                <a
+                  target="_blank"
+                  :href="item.link"
+                  class="resumeViewItem__content-link"
+                >{{item.link}}</a>
+              </div>
+            </div>
+            <div class="resumeViewItem resumeViewItem-desc">
+              <div class="resumeViewItem__label">描述</div>
+              <div class="resumeViewItem__content">{{item.description}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 获奖 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">获奖</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.award_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">获奖名称</div>
+              <div class="resumeViewItem__content">{{item.title}}</div>
+            </div>
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">获奖时间</div>
+              <div class="resumeViewItem__content">{{item.award_time | formatDate(false)}}</div>
+            </div>
+            <div class="resumeViewItem resumeViewItem-desc">
+              <div class="resumeViewItem__label">描述</div>
+              <div class="resumeViewItem__content">{{item.desc}}</div>
+            </div>
+          </div>
+        </div>
+        <!-- 语言 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">语言能力</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.language_skill_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">语言</div>
+              <div class="resumeViewItem__content">{{item.language}}</div>
+            </div>
+             
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">精通程度</div>
+              <div class="resumeViewItem__content">{{item.proficiency}}</div>
+            </div>
+             
+          </div>
+        </div>
+
+           <!-- 自我评价 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">自我评价</h2>
+
+          <div class="resumeViewForm">
+            
+            
+            <div class="resumeViewItem resumeViewItem-desc">
+              {{resumeDetail.self_evaluation}}
+            </div>
+          </div>
+        </div>
+
+            <!-- 社交账号 -->
+        <div class="resumeViewSection">
+          <h2 class="resumeViewSection__title">社交账号</h2>
+
+          <div
+            :key="index"
+            v-for="(item,index) in resumeDetail.sns_list"
+            class="resumeViewForm clearfix"
+          >
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">社交平台</div>
+              <div class="resumeViewItem__content">{{item.sns_type}}</div>
+            </div>
+             
+            <div class="resumeViewItem">
+              <div class="resumeViewItem__label">URL/ID</div>
+              <div class="resumeViewItem__content">{{item.link}}</div>
+            </div>
+             
+          </div>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 <script>
 import { fetchResume } from "@/helper/requestWithToken.js";
+// const snsMap={}
 export default {
   name: "resume",
   data() {
@@ -36,13 +299,9 @@ export default {
     };
   },
 
-  
-  methods: {
-   
-  },
+  methods: {},
   created() {
     fetchResume().then(res => {
-      
       this.resumeDetail = res.data.resume_detail;
     });
   }
@@ -70,34 +329,42 @@ export default {
     align-items: center;
   }
 
-  &-content {
+  .resumeForm {
     margin-top: 30px;
     background: #fff;
     box-shadow: 0px 9px 20px 0px #c2c1c180;
     border-radius: 4px;
     padding: 30px;
 
-    // &-block {
-    //   min-height: 200px;
-    //   margin-bottom: 30px;
-    //   &__title {
-    //     margin-bottom: 20px;
-    //   }
-    //   &-grid {
-    //     float: left;
-    //     width: 50%;
-    //     margin-bottom: 30px;
-    //     &__label {
-    //       color: @secondary-text-color;
-    //       font-size: @font-size-base;
-    //       margin-bottom: 7px;
-    //     }
-    //   }
-
-    //   &Desc{
-    //     width:70%;
-    //   }
-    // }
+    .resumeViewSection {
+      min-height: 200px;
+      margin-bottom: 30px;
+      &__title {
+        margin-bottom: 20px;
+      }
+      .resumeViewForm {
+        margin-bottom: 30px;
+      }
+      .resumeViewItem {
+        float: left;
+        width: 50%;
+        margin-bottom: 20px;
+        min-height: 50px;
+        &__label {
+          color: @secondary-text-color;
+          font-size: @font-size-base;
+          margin-bottom: 7px;
+        }
+        &__content-link {
+          color: @main-color;
+        }
+        &-desc {
+          overflow: hidden;
+          width: 70%;
+          white-space: pre-line;
+        }
+      }
+    }
   }
 }
 </style>
