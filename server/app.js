d1f6cda53a6159a915e603657ffe331afada361d
konglingwen94 <794430774@qq.com>
2020-08-20 09:13:17 +0800

    添加文件上传代理服务中间件

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const proxyOpts = {
   target: "https://job.bytedance.com",
   changeOrigin: true,
   cookieDomainRewrite: "", //去除cookie的默认携带域名
+  // cookieDomainRewrite: ".job.bytedance.com", //去除cookie的默认携带域名
 
   onProxyRes(proxyResponse, req, res) {
     if (proxyResponse.headers["set-cookie"]) {
@@ -20,11 +21,34 @@ const proxyOpts = {
         return cookie.replace(/; secure/gi, "").replace(/; httpOnly/gi, "");
       });
       proxyResponse.headers["set-cookie"] = cookies;
+      // proxyResponse.headers["set-cookie"] = "atsx-csrf-token=ADVfxg4aVw0_odc7KFbmjheiLFlHVNlSAjyWAu5dqMg=; Path=/; Max-Age=604800";
     }
   },
 };
-// 使用代理服务转发请求
+// 使用代理服务转发API请求
 app.use("/api/v1", createProxyMiddleware(proxyOpts));
+// 
+// 使用代理服务转发文件上传接口请求
+app.use("/atsx/blob", createProxyMiddleware({
+  changeOrigin:true,
+  target:'https://job.bytedance.com',
+  onProxyReq(proxyReq,req,res){
+    console.clear()
+    console.log(new Date().toISOString(),req.path)
+    // debugger
+    // Object.keys(req.headers).map((key) => {
+    //   req.headers[key]= req.headers[key].replace(
+    //   "http://localhost:8080",
+    //   "https://job.bytedance.com"
+    // );
+  // });
+  
+    // console.log(proxyReq.headers)
+  },
+  // onProxyRes(){
+  //   console.log('onProxyRes')
+  // }
+}));
 
 app.use(bodyParser());
 // 自定义路由转发
