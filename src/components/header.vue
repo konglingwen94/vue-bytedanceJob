2ece87935631d0c27b667000ed0f56566406d0b9
konglingwen94 <794430774@qq.com>
2020-09-04 21:18:18 +0800

    添加github导链

--- src/components/header.vue
+++ src/components/header.vue
@@ -21,9 +21,8 @@
           <a :href="href" @click="navigate">产品与服务</a>
         </li>
       </router-link>
-
       <li class="navbar-item">
-        <a href="https://job.bytedance.com/campus/" target="_blank">校园招聘</a>
+        <a href="https://job.bytedance.com/campus" target="_blank">校园招聘</a>
       </li>
     </ul>
     |
@@ -47,6 +46,25 @@
         </ul>
       </div>
     </div>
+    <div class="github-project">
+      <a
+        href="https://github.com/konglingwen94/vue-bytedanceJob"
+        target="_blank"
+      >
+        <svg
+          class="octicon octicon-mark-github v-align-middle"
+          height="32"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="32"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path></svg
+      ></a>
+    </div>
   </div>
 </template>
 <script>
@@ -54,47 +72,47 @@ import store from "@/store";
 export default {
   name: "Header",
   data: () => ({
-    state: store.state
+    state: store.state,
   }),
   created() {
     store
       .requestLoginStatus()
-      .then(isLogin => {
+      .then((isLogin) => {
         if (!isLogin) {
           store.expireLogin();
         } else if (isLogin && !this.state.userInfo.email) {
           store
             .requestUserInfo()
-            .then(res => {})
-            .catch(err => {});
+            .then((res) => {})
+            .catch((err) => {});
         }
       })
-      .catch(err => {
+      .catch((err) => {
         store.expireLogin();
       });
   },
   props: {
     fixedToTop: {
       type: Boolean,
-      default: false
+      default: false,
     },
     themeColor: {
       type: String,
-      default: "main-color"
-    }
+      default: "main-color",
+    },
   },
   methods: {
     handleLogout() {
       store
         .requestLogout()
-        .then(res => {
+        .then((res) => {
           this.$router.push("/");
         })
-        .catch(err => {
+        .catch((err) => {
           throw err;
         });
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
@@ -211,5 +229,9 @@ export default {
       }
     }
   }
+}
+.github-project {
+  margin-left: 30px;
+  margin-right: -30px;
 }
 </style>
